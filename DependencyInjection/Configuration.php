2e63e7f90ea3bc9c3b6305139ca8f9b738740a1f
Tomasz Struczyński <tomasz.struczynski@socializer.pl>
2015-12-29 16:51:50 +0100

    Removed unnecessary comments.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,10 +21,6 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('gendoria_param_converter');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
